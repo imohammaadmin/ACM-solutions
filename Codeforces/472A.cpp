1dfe8fb8b67349c4456a4e329292df0ab84fab98
imohammaadmin <mav.ipad@live.com>
2015-01-16 19:33:22 +0330

    Design Tutorial: Learn from Math

--- Codeforces/472A.cpp
+++ Codeforces/472A.cpp
@@ -16,9 +16,9 @@ int MohammadAmin{
     int n;
     cin >> n;
     if ( n % 2 == 0 ){
-        cout << 8 << n - 8;
+        cout << 8 << " " << n - 8;
     }else{
-        cout << 9 << n - 9;
+        cout << 9 << " " << n - 9;
     }
 	return 0;
 }
